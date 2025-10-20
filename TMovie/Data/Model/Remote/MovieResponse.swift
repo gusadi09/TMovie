8460018f5ce9f27f4d925343bfab56dcea4fd460
AgusAdiPranata <agus@ewideplus.com>
2025-10-20 15:09:18 +0700

    [SEARCH]<FEATURE> add remote movie model

--- TMovie/Data/Model/Remote/MovieResponse.swift
+++ TMovie/Data/Model/Remote/MovieResponse.swift
@@ -7,6 +7,119 @@
 
 import Foundation
 
-struct MovieResponse: Codable, Identifiable, Hashable {
-	let id: UUID
+enum RemoteMovie {
+	enum Response {}
+	enum Request {}
+}
+
+extension RemoteMovie.Response {
+	struct Detail: Codable, Identifiable, Hashable {
+		let id: UInt?
+		let adult: Bool?
+		let backdropPath: String?
+		let budget: UInt?
+		let genres: [Genre]
+		let homepage: URL?
+		let imdbId: String?
+		let originalLanguage: String?
+		let originalTitle: String?
+		let overview: String?
+		let popularity: Float?
+		let posterPath: String?
+		let productionCompanies: [ProductionCompany]
+		let productionCountries: [ProductionCountry]
+		let releaseDate: Date?
+		let revenue: UInt?
+		let runtime: UInt?
+		let spokenLanguages: [SpokenLanguage]
+		let status: String?
+		let tagline: String?
+		let title: String?
+		let video: Bool?
+		let voteAverage: Float?
+		let voteCount: UInt?
+		
+		enum CodingKeys: String, CodingKey {
+			case id
+			case adult
+			case backdropPath = "backdrop_path"
+			case budget
+			case genres
+			case homepage
+			case imdbId
+			case originalLanguage = "original_language"
+			case originalTitle = "original_title"
+			case overview
+			case popularity
+			case posterPath = "poster_path"
+			case productionCompanies = "production_companies"
+			case productionCountries = "production_countries"
+			case releaseDate = "release_date"
+			case revenue
+			case runtime
+			case spokenLanguages = "spoken_languages"
+			case status
+			case tagline
+			case title
+			case video
+			case voteAverage = "vote_average"
+			case voteCount = "vote_count"
+		}
+	}
+	
+	struct Genre: Codable, Identifiable, Hashable {
+		let id: UInt?
+		let name: String?
+	}
+	
+	struct ProductionCompany: Codable, Identifiable, Hashable {
+		let id: UInt?
+		let logoPath: String?
+		let name: String?
+		let originCountry: String?
+		
+		enum CodingKeys: String, CodingKey {
+			case id
+			case logoPath = "logo_path"
+			case name
+			case originCountry = "origin_country"
+		}
+	}
+	
+	struct ProductionCountry: Codable, Hashable {
+		let iso3166_1: String?
+		let name: String?
+		
+		enum CodingKeys: String, CodingKey {
+			case iso3166_1 = "iso_3166_1"
+			case name
+		}
+	}
+	
+	struct SpokenLanguage: Codable, Hashable {
+		let englishName: String?
+		let iso639_1: String?
+		let name: String?
+		
+		enum CodingKeys: String, CodingKey {
+			case englishName = "english_name"
+			case iso639_1 = "iso_639_1"
+			case name
+		}
+	}
+}
+
+extension RemoteMovie.Request {
+	struct Search: Equatable {
+		var query: String
+		var page: UInt
+	}
+	
+	struct Detail: Equatable {
+		var movieId: UInt
+		
+		enum CodingKeys: String, CodingKey {
+			case movieId = "movie_id"
+		}
+	}
 }
